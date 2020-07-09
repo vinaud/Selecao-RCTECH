a4cc3751028758339ca71a7c659c4db23afbe018
João Vinaud <leyenasd@gmail.com>
2020-07-08 23:24:32 -0300

    consumo da lista de filmes

--- movies/movies/spiders/movietable.py
+++ movies/movies/spiders/movietable.py
@@ -4,7 +4,16 @@ import scrapy
 class MovietableSpider(scrapy.Spider):
     name = 'movietable'
     allowed_domains = ['pastebin.com']
-    start_urls = ['https://pastebin.com/PcVfQ1ff/', 'https://pastebin.com/Tdp532rr']
+    start_urls = ['https://pastebin.com/PcVfQ1ff/']
 
     def parse(self, response):
-        print(response.status)
+        all_movies = response.xpath('//tbody')
+
+        for movie in all_movies:
+            genero = movie.xpath('.//tr/td/').extract_first()
+           
+            print(genero)
+        
+
+        print('Response status:', response.status)
+        print(response.txt)
